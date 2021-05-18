13bffa18022096ac1d422af7791a044c218ec79b
Benvorth <benjamin.steinvorth@gmail.com>
2021-05-18 10:27:37 +0200

    change(): Notifications

--- src/main/resources/static/sw-cache.js
+++ src/main/resources/static/sw-cache.js
@@ -1,15 +1,27 @@
 /* This service worker manages the device-local cache */
 
-let cacheName = 'PUSHr';
+let cacheName = 'PUSHr-v1';
 let filesToCache = [
     '/', /* so browser caches index.html even if user didn't directly type in that */
     '/index.html',
     '/css/style.css',
+    '/img/pushr-24.png',
+    '/img/pushr-36.png',
+    '/img/pushr-48.png',
+    '/img/pushr-72.png',
+    '/img/pushr-96.png',
+    '/img/pushr-128.png',
+    '/img/pushr-144.png',
+    '/img/pushr-152.png',
+    '/img/pushr-192.png',
+    '/img/pushr-256.png',
+    '/img/pushr-512.png',
     '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install cache...')
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             return cache.addAll(filesToCache);
@@ -24,4 +36,14 @@ self.addEventListener('fetch', function (e) {
             return response || fetch(e.request);
         })
     );
+});
+
+/* Delete old cache versions */
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        Promise.all(keyList.map((key) => {
+            if (key === cacheName) { return;}
+            caches.delete(key);
+        }))
+    }));
 });
