be8a0b5201d1af0d403f7a0ba67f1dabe1bbba4c
Benvorth <benjamin.steinvorth@gmail.com>
2021-05-18 10:29:32 +0200

    change(): Notifications

--- src/main/resources/static/sw-cache.js
+++ src/main/resources/static/sw-cache.js
@@ -1,6 +1,6 @@
 /* This service worker manages the device-local cache */
 
-let cacheName = 'PUSHr-v1';
+let cacheName = 'PUSHr-v2';
 let filesToCache = [
     '/', /* so browser caches index.html even if user didn't directly type in that */
     '/index.html',
@@ -20,19 +20,19 @@ let filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install cache...')
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open(cacheName).then((cache) => {
             return cache.addAll(filesToCache);
         })
     );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then(function (response) {
+        caches.match(e.request).then((response) => {
             return response || fetch(e.request);
         })
     );
