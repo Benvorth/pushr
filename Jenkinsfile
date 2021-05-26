39b7a1c98bef823b7ed01405c14eb66720a47e20
Benvorth <benjamin.steinvorth@gmail.com>
2021-05-26 13:49:35 +0200

    change(frontend): integration into react frontend

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,17 +1,36 @@
 #!/usr/bin/env groovy
 
 node {
-    stage 'Clone the project'
-    git branch: 'main', url: 'https://github.com/Benvorth/pushr.git'
 
-    stage("Compilation") {
+    dir('pushr-fe') {
+
+        stage('Clone the frontend') {
+            git branch: 'main', url: 'https://github.com/Benvorth/pushr-fe.git'
+        }
+
+        stage('Build the frontend') {
+            sh "npm run package"
+        }
+    }
+
+
+    stage ('Clone the backend') {
+        git branch: 'main', url: 'https://github.com/Benvorth/pushr.git'
+    }
+
+    stage('Merge frontend and backend') {
+        sh "cp -r pushr-fe/build src/main/resources/static"
+    }
+
+    stage("Build the backend") {
         sh "mvn clean install -DskipTests"
     }
 
-    stage("Deployment") {
-
+    stage("Stop running instance") {
         sh "pid=\$(lsof -i:8081 -t) || true; kill -TERM \$pid || kill -KILL \$pid || true"
+    }
 
+    stage("Deploy new version") {
         withEnv(['JENKINS_NODE_COOKIE=dontkill']) {
             sh 'nohup mvn spring-boot:run -Dserver.port=8081 &'
         }
--- src/main/java/de/benvorth/pushr/pushService/PushController.java
+++ src/main/java/de/benvorth/pushr/pushService/PushController.java
@@ -15,23 +15,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
-@RequestMapping(path="api")
+@RequestMapping(path = "api")
 public class PushController {
 
     private final RestTemplate httpClient;
@@ -56,22 +60,54 @@ public class PushController {
             this.serverKeys.getPrivateKey());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(path = "/publicSigningKey", produces = "application/octet-stream")
     public byte[] publicSigningKey() {
         return this.serverKeys.getPublicKeyUncompressed();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(path = "/publicSigningKeyBase64")
     public String publicSigningKeyBase64() {
         return this.serverKeys.getPublicKeyBase64();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/subscribe")
     @ResponseStatus(HttpStatus.CREATED)
-    public void subscribe(@RequestBody Subscription subscription) {
+    public String subscribe(@RequestBody Subscription subscription) {
         this.subscriptions.put(subscription.getEndpoint(), subscription);
+        String subscriptionId = createHash(subscription);
+        return "{\"subscriptionId\":\"" + subscriptionId + "\"}";
     }
 
+    private String createHash(Subscription subscription) {
+        byte[] bytesOfMessage = new byte[0];
+        String hashtext = "";
+        try {
+            bytesOfMessage = subscription.toString().getBytes("UTF-8");
+            MessageDigest md = null;
+            md = MessageDigest.getInstance("MD5");
+
+            md.reset();
+            md.update(bytesOfMessage);
+            byte[] thedigest = md.digest();
+            BigInteger bigInt = new BigInteger(1, thedigest);
+            hashtext = bigInt.toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }  catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // Now we need to zero pad it if you actually want the full 32 chars.
+        while(hashtext.length() < 32 ){
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/subscribeAngular")
     @ResponseStatus(HttpStatus.CREATED)
     public void subscribeAngular(@RequestBody Subscription subscription) {
@@ -79,11 +115,13 @@ public class PushController {
         this.subscriptionsAngular.put(subscription.getEndpoint(), subscription);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/unsubscribe")
     public void unsubscribe(@RequestBody SubscriptionEndpoint subscription) {
         this.subscriptions.remove(subscription.getEndpoint());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/unsubscribeAngular")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unsubscribeAngular(@RequestBody SubscriptionEndpoint subscription) {
@@ -91,24 +129,51 @@ public class PushController {
         this.subscriptionsAngular.remove(subscription.getEndpoint());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/isSubscribed")
     public boolean isSubscribed(@RequestBody SubscriptionEndpoint subscription) {
         return this.subscriptions.containsKey(subscription.getEndpoint());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/isSubscribedAngular")
     public boolean isSubscribedAngular(@RequestBody SubscriptionEndpoint subscription) {
         return this.subscriptionsAngular.containsKey(subscription.getEndpoint());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(path = "/lastNumbersAPIFact")
     public String lastNumbersAPIFact() {
         return this.lastNumbersAPIFact;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/sendTextNotification")
+    public boolean sendTextNotification(
+        @RequestParam("subscriptionEndpoint") String subscriptionEndpoint,
+        @RequestBody String message
+    ) {
+
+        Subscription subscription = this.getSubscriptionByEndpoint(subscriptionEndpoint);
+        if (subscription != null) {
+            return this.sendTextPushMessage(subscription, new PushMessage("Text Notification", message));
+        } else {
+            return false;
+        }
+    }
+
     @Scheduled(fixedDelay = 20_000)
     public void numberFact() {
         if (this.subscriptions.isEmpty()) {
+            return;
+        }
+
+        if (true) {
+            this.lastNumbersAPIFact = "This is a static fact.";
+            sendPushMessageToAllSubscribers(this.subscriptions,
+                new PushMessage("A Joke", "hahaha")
+            );
+            // sendPushMessageToAllSubscribersWithoutPayload();
             return;
         }
 
@@ -123,7 +188,7 @@ public class PushController {
         }
     }
 
-    @Scheduled(fixedDelay = 30_000)
+    // @Scheduled(fixedDelay = 30_000)
     public void chuckNorrisJoke() {
         if (this.subscriptions.isEmpty() && this.subscriptionsAngular.isEmpty()) {
             return;
@@ -200,6 +265,33 @@ public class PushController {
         failedSubscriptions.forEach(subs::remove);
     }
 
+    private boolean sendTextPushMessage(Subscription subscription, Object message) {
+
+        /*
+        Notification notification = new Notification("Custom Text Notification");
+        notification.setBody(messageBody);
+        // notification.setIcon("assets/chuck.png");
+
+        Map<String, Notification> singletonMap = new HashMap<>();
+        singletonMap.put("notification", notification);
+        */
+        byte[] result = null;
+        try {
+            /*result = this.cryptoService.encrypt(
+                this.objectMapper.writeValueAsString(singletonMap),
+                subscription.getKeys().getP256dh(), subscription.getKeys().getAuth(), 0);
+             */
+            result = this.cryptoService.encrypt(
+                this.objectMapper.writeValueAsString(message),
+                subscription.getKeys().getP256dh(), subscription.getKeys().getAuth(), 0);
+
+        } catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException | InvalidAlgorithmParameterException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | JsonProcessingException e) {
+            PushrApplication.logger.error("Error encrypting message: ", e);
+        }
+
+        return sendPushMessage(subscription, result);
+    }
+
     /**
      * @return true if the subscription is no longer valid and can be removed, false if
      * everything is okay
@@ -252,8 +344,36 @@ public class PushController {
         //    .build();
 
 
-        ResponseEntity<String> response = this.httpClient.postForEntity(
-            endpointURI, entity, String.class);
+        ResponseEntity<String> response = null;
+        try {
+            response = this.httpClient.postForEntity(
+                endpointURI, entity, String.class);
+        } catch (HttpClientErrorException e) {
+
+            switch (e.getRawStatusCode()) {
+
+                case 404:
+                case 410:
+                    PushrApplication.logger.warn("Subscription not found or gone: {}",
+                        subscription.getEndpoint());
+                    // remove subscription from our collection of subscriptions
+                    removeSubscription(subscription);
+                    return true;
+                case 429:
+                    PushrApplication.logger.error("Too many requests: {}", response.toString());
+                    break;
+                case 400:
+                    PushrApplication.logger.error("Invalid request: {}", response.toString());
+                    break;
+                case 413:
+                    PushrApplication.logger.error("Payload size too large: {}", response.toString());
+                    break;
+                default:
+                    PushrApplication.logger.error("Unhandled status code: {} / {}",
+                        e.getRawStatusCode(), e.toString());
+            }
+            return false;
+        }
 
         // HttpResponse<Void> response = this.httpClient.send(request,
         //     HttpResponse.BodyHandlers.discarding());
@@ -285,5 +405,18 @@ public class PushController {
 
 
         return false;
+    }
+
+    private void removeSubscription(Subscription subscription) {
+        this.subscriptions.remove(subscription.getEndpoint());
+    }
+
+    private Subscription getSubscriptionByEndpoint(String subscriptionEndpoint) {
+        for (Subscription subscription : this.subscriptions.values()) {
+            if (subscription.getEndpoint().equals(subscriptionEndpoint)) {
+                return subscription;
+            }
+        }
+        return null;
     }
 }
