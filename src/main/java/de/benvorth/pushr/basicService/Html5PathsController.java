da77c283d96f4c7148cb1035c9089a0a29f07834
Benvorth <benjamin.steinvorth@gmail.com>
2021-05-27 09:38:14 +0200

    change(jenkinsfile): explicitly delete old static folder

---
+++ src/main/java/de/benvorth/pushr/basicService/Html5PathsController.java
@@ -0,0 +1,16 @@
+package de.benvorth.pushr.basicService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// ensure that all page-requests are forwarded to index.html (client-side routing)
+// https://stackoverflow.com/questions/44692781/configure-spring-boot-to-redirect-404-to-a-single-page-app
+@Controller
+public class Html5PathsController {
+
+    // match all paths that do not contain a period and are not already mapped to another controller.
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
