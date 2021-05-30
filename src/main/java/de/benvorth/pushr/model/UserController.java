0f34da200511da3b541411838d7e836da0240fd3
Benvorth <benjamin.steinvorth@gmail.com>
2021-05-30 22:00:16 +0200

    user controller api

--- src/main/java/de/benvorth/pushr/model/UserController.java
+++ src/main/java/de/benvorth/pushr/model/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api")
 public class UserController {
 
     UserRepository userRepository;
